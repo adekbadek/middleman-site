20d3032c14e92c230bb6638827679b21a8e435ee
Adam Borowski <adam@adamboro.com>
2016-01-14 13:01:20 +0100

    Initial commit

---
+++ config.rb
@@ -0,0 +1,52 @@
+###
+# Page options, layouts, aliases and proxies
+###
+
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload, apply_css_live: false, apply_js_live: false
+end
+
+# tell Middleman to create a folder for each .html file and place the built template file as the index of that folder
+activate :directory_indexes
+
+activate :autoprefixer
+
+###
+# Helpers
+###
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+# Build-specific configuration
+configure :build do
+  # Use relative URLs
+  activate :relative_assets
+
+  # Minify CSS on build
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+end
