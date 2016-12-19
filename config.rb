e37363440eaebcab6fded9ce5c319dd98119ed8b
Adam Borowski <adam@adamboro.com>
2016-12-19 17:26:08 +0100

    clean up middleman config

--- config.rb
+++ config.rb
@@ -9,13 +9,6 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
 # General configuration
 
 # Reload the browser automatically whenever files change
@@ -37,13 +30,6 @@ activate :external_pipeline,
 ###
 # Helpers
 ###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 # Build-specific configuration
 configure :build do
